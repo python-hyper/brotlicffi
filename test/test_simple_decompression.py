ac0d207e9392e73d30b14e4c0b737067748fba96
Cory Benfield <lukasaoz@gmail.com>
2015-10-02 18:18:25 +0100

    Test drip feeding.

--- test/test_simple_decompression.py
+++ test/test_simple_decompression.py
@@ -33,3 +33,23 @@ def test_decompressobj(simple_compressed_file):
     data += o.flush()
 
     assert data == uncompressed_data
+
+
+def test_drip_feed(simple_compressed_file):
+    """
+    Sending in the data one byte at a time still works.
+    """
+    with open(simple_compressed_file[0], 'rb') as f:
+        uncompressed_data = f.read()
+
+    with open(simple_compressed_file[1], 'rb') as f:
+        compressed_data = f.read()
+
+    outdata = []
+    o = brotli.brotli.Decompressor()
+    for i in range(0, len(compressed_data)):
+        outdata.append(o.decompress(compressed_data[i:i+1]))
+
+    outdata.append(o.flush())
+
+    assert b''.join(outdata) == uncompressed_data
