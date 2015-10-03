1be74a8e8eb9f1a2a8b0116cdb56bb9357763b90
Cory Benfield <lukasaoz@gmail.com>
2015-10-03 18:09:48 +0100

    v0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="brotlipy",
-    version="0.1.1",
+    version="0.1.2",
 
     description="Python binding to the Brotli library",
     long_description=open("README.rst").read(),
