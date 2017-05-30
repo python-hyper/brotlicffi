da3be6051386791ea7f5372230caecc2328262ff
Cory Benfield <lukasaoz@gmail.com>
2017-05-30 09:20:01 +0100

    v0.7.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (
 
 setup(
     name="brotlipy",
-    version="0.6.0",
+    version="0.7.0",
 
     description="Python binding to the Brotli library",
     long_description=long_description,
